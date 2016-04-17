eeffe0bfad98d28e131008c99ce0521b83e96ae8
Eb <ibbad@users.noreply.github.com>
2016-04-17 17:04:34 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -7,3 +7,19 @@ function palindrome(str) {
   return str == reversed;
 }
 
+function factorialize(num) {
+  var factorial=1;
+  for (var i=num; i>0; i--){
+    factorial *= i;
+  }
+  return factorial;
+}
+
+function reverseString(str) {
+  var reversed = "";
+  for(var i=str.length;i>0;i--){
+    reversed+=str[i-1];
+  }
+  return reversed;
+}
+
