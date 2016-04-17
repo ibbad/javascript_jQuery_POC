3e4ce3bbe7ae373ac3c4c4e80d9e0dc0d200307d
Eb <ibbad@users.noreply.github.com>
2016-04-17 17:15:45 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -6,6 +6,7 @@ function palindrome(str) {
   }
   return str == reversed;
 }
+palindrome('Eye');
 
 function factorialize(num) {
   var factorial=1;
@@ -14,6 +15,7 @@ function factorialize(num) {
   }
   return factorial;
 }
+factorialize(1234);
 
 function reverseString(str) {
   var reversed = "";
@@ -22,4 +24,42 @@ function reverseString(str) {
   }
   return reversed;
 }
+reverseString("Potato");
 
+function findLongestWord(str) {
+  var words = str.split(' ');
+  var lngst = words[0].length;
+  for (var i = 0; i < words.length; i++){
+    if (words[i].length > lngst){
+      lngst = words[i].length;
+    }
+  }
+  return lngst;
+}
+findLongestWord("The quick brown fox jumped over the lazy dog");
+
+function titleCase(str) {
+  var words = str.split(' ');
+  for (var i=0;i<words.length; i++){
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].substr(1).toLowerCase();
+  }
+  return words.join(' ');
+}
+
+titleCase("I'm a little tea pot");
+
+function largestOfFour(arr) {
+  var largest = [];
+  for (var i=0; i< arr.length; i++){
+    var l = arr[i][0];
+    for (var j =0; j<arr[i].length; j++){
+      if (arr[i][j]>l){
+        l=arr[i][j];
+      }
+    }
+    largest.push(l);
+  }
+  return largest;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
