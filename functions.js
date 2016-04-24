5e5540211a33680d8e2fd83475109011bbc80d3e
Eb <ibbad@users.noreply.github.com>
2016-04-24 15:04:42 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -313,3 +313,10 @@ function pairElement(str) {
 }
 pairElement("GCG");
 
+
+function uniteUnique() {
+    return [].concat.apply([], arguments).filter(function(elem, index, self) {
+        return self.indexOf(elem) === index;
+    });
+}
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
