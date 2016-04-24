4befcb9c4c114bc4aa082652a380dbb0ac2d8e9a
Eb <ibbad@users.noreply.github.com>
2016-04-24 15:42:05 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -354,3 +354,22 @@ function convertHTML(str) {
 }
 convertHTML("Dolce & Gabbana");
 convertHTML("Hamburgers < Pizza < Tacos");
+
+
+function sumFibs(num) {
+    var prevNumber = 0;
+    var currNumber = 1;
+    var result = 0;
+    while (currNumber <= num) {
+        if (currNumber % 2 !== 0) {
+            result += currNumber;
+        }
+        var added = currNumber + prevNumber;
+        prevNumber = currNumber;
+        currNumber = added;
+    }
+
+    return result;
+}
+sumFibs(1000);
+
